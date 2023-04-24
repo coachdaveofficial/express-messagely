59b677491b8cc125b49724a263bcc1dc21988db0
David Falson <david@falson.net>
2023-04-23 19:43:56 -0700

    add route logic to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
+const express = require("express");
+const router = new express.Router();
+const User = require("../models/user");
+const {ensureCorrectUser, ensureLoggedIn} = require("../middleware/auth")
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+    try {
+        const users = await User.all();
+        return res.json(users);
+    } catch (e) {
+        return next(e);
+    }
+});
 
 
 /** GET /:username - get detail of users.
@@ -10,7 +24,14 @@
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const user = User.get(req.param.username)
+        return res.json(user);
+    } catch (e) {
+        return next(e);        
+    }
+})
 
 /** GET /:username/to - get messages to user
  *
@@ -21,6 +42,14 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const messages = User.messagesTo(req.param.username);
+        return res.json(messages);
+    } catch (e) {
+        return next(e);
+    }
+})
 
 
 /** GET /:username/from - get messages from user
@@ -32,3 +61,11 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get('/:username/from', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const messages = User.messagesFrom(req.param.username);
+        return res.json(messages);
+    } catch (e) {
+        return next(e);
+    }
+})
